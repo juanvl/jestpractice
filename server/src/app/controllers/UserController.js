e7735717b718ccbc1b1f1973302af9d3c7dc9f44
Juan <jnvleite@gmail.com>
2019-10-23 11:20:10 -0300

    configs and more tests

--- server/src/app/controllers/UserController.js
+++ server/src/app/controllers/UserController.js
@@ -2,7 +2,16 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const { email } = req.body;
+
+    const userExists = await User.findOne({ where: { email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: `User ${email} already exists` });
+    }
+
     const user = await User.create(req.body);
+
     return res.json(user);
   }
 }
