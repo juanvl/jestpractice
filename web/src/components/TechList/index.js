a889852b4e8ae48a416b6b9528597b459135ae0d
Juan <jnvleite@gmail.com>
2020-01-21 10:00:07 -0300

    form test

--- web/src/components/TechList/index.js
+++ web/src/components/TechList/index.js
@@ -1,30 +1,32 @@
 import React, { useState } from 'react';
 
-// import { Container } from './styles';
-
 export default function TechList() {
-  // const [newTech, setNewTech] = useState('');
   const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
 
   function handleAddTech() {
-    setTechs([...techs, 'Node.js']);
+    setTechs([...techs, newTech]);
+    setNewTech('')
   }
 
   return (
-    <div>
+    <form data-testid="tech-form" onSubmit={handleAddTech}>
       <ul data-testid="tech-list">
-        {techs.map(t => (
-          <li key={t}>{t}</li>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
         ))}
       </ul>
-      {/* <input
-        type="text"
+
+      <label htmlFor="tech">Tech</label>
+      <input
+        id="tech"
         value={newTech}
         onChange={e => setNewTech(e.target.value)}
-      /> */}
-      <button type="button" onClick={handleAddTech}>
+      />
+
+      <button onClick={handleAddTech}>
         Adicionar
       </button>
-    </div>
+    </form>
   );
 }
